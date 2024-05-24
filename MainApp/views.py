2dedf85ad5931f39782d033fc88e47aa51fc1091
Torero379 <i.kipelov@mail.ru>
2024-05-25 02:06:22 +0300

    task 6

--- MainApp/views.py
+++ MainApp/views.py
@@ -23,38 +23,38 @@ items = [
 {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
 {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
 {"id": 7, "name": "Картофель фри" ,"quantity":0},
-{"id": 8, "name": "Кепка" ,"quantity":124},
-{"id": 10, "name": "Кепка" ,"quantity":124},]
+{"id": 8, "name": "Кепка" ,"quantity":124}
+]
 
-
-def item1 (request,item):
+def item1 (request, item):
     count_1 = 0
     for nec_ in items:
         count_1 += 1
         if nec_.get("id") == item:
-            return HttpResponse(f'{nec_.get("name")} &nbsp {nec_.get("quantity")} штук')
+            return HttpResponse(f'{nec_.get("name")} &nbsp {nec_.get("quantity")} штук <a href="http://127.0.0.1:8000/items">Назад к списку товаров</a>')
         if count_1 > len(items): break
     if item == 10:
         for nec_ in items:
             count_1 += 1      
             if nec_.get("id") == 10:
-                return HttpResponse(f'{nec_.get("name")} &nbsp {nec_.get("quantity")} штук')
+                return HttpResponse(f'{nec_.get("name")} &nbsp {nec_.get("quantity")} штук <a href="http://127.0.0.1:8000/items">Назад к списку товаров</a>')
             if nec_.get("id") != 10:
-                return HttpResponse("<h1> Товар c id=10 не найден </h1>")
+                return HttpResponse('<h1> Товар c id=10 не найден <a href="http://127.0.0.1:8000/items">Назад к списку товаров</a></h1>')
             if count_1 > len(items): break
+
 def all_items (request):
     count_1 = 0
-    list1 = ["<ol>"]
+    list_names = ["<ol>"]    
+    necid = 0
     for nec_ in items:  
         count_1 += 1
-        list1.append("<li>")
-        list1.append(nec_.get("name"))
-        list1.append("</li>")
-
-
+        list_names.append("<li>")
+        necid = nec_.get("id")
+        list_names.append(nec_.get("name"))
+        list_names.append(f"<a href='http://127.0.0.1:8000/item/{necid}'>Страница товара</a></li>")
         if count_1 > len(items): break
-    list1.append("</ol>")
-    return HttpResponse(list1)
+    list_names.append("</ol>")
+    return HttpResponse(list_names)
 
 
         
